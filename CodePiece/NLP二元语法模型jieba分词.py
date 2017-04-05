65745a9c21e8ebbcbf841701463f2155b25f610a
Fire_Yw <fire15@126.com>
2017-04-05 15:48:46 +0800

    Update NLP二元语法模型jieba分词.py

--- CodePiece/NLP二元语法模型jieba分词.py
+++ CodePiece/NLP二元语法模型jieba分词.py
@@ -1,36 +1,68 @@
 #coding:utf-8
-
 import jieba
 
+ori_data = u'刘超是一个喜欢学习的好学生，你看，他正在学习呢。除了学习刘超还喜欢打游戏。'
+print ori_data
 
-#将句子变为"BOSxxxxxEOS"这种形式
 def reform(sentence):
     #如果是以“。”结束的则将“。”删掉
-    if sentence.endswith("。"):
+    if sentence[-1] == u"。":
         sentence=sentence[:-1]
-    #添加起始符BOS和终止符EOS   
-    sentence_modify1=sentence.replace("。", "EOSBOS")
-    sentence_modify2="BOS"+sentence_modify1+"EOS"
-    return sentence_modify2
+    #添加起始符BOS和终止符EOS 
+    tmp = [u'、' ,u'，',u'：',u'。']
+    for i in xrange(len(sentence)):
+        if sentence[i] in tmp:
+            sentence=sentence[:i]+'EEEBBB'+sentence[i+1:]
+    sentence2="BBB"+sentence+"EEE"
+    return sentence2
 
+data1 = reform(ori_data)
+print data1
 
 #分词并统计词频
-def segmentation(sentence,lists,dicts={}):
-    jieba.suggest_freq("BOS", True)
-    jieba.suggest_freq("EOS", True)
-    sentence = jieba.cut(sentence,HMM=False)
+def segmentation(sentence,lists=[],dicts={}):
+    jieba.suggest_freq(u"BBB", True)
+    jieba.suggest_freq(u"EEE", True)
+    jieba.suggest_freq(u"刘超", True)
+    #分词
+    sentence = jieba.cut(sentence,cut_all=False,HMM=False)
+    #组合
     format_sentence=",".join(sentence)
-    #将词按","分割后依次填入数组word_list[]
-    lists=format_sentence.split(",")
-    #统计词频，如果词在字典word_dir{}中出现过则+1，未出现则=1
-    if dicts is not None:
-        for word in lists:
-            if word not in dicts:
-                dicts[word]=1
-            else:
-                dicts[word]+=1               
+    lists=format_sentence.split(",")     
+    for word in lists:
+        if word not in dicts:
+            dicts[word]=1
+        else:
+            dicts[word]+=1 
     return lists
 
+
+dict1 = {}
+t = segmentation(data1,lists=[],dicts=dict1)
+for x in t:
+    print x.encode('utf-8')
+#输出词频，同时去除一些杂词  
+badwords = []
+#badwords = [u'的',u'是',u'呢',u'还',u'BBB',u'EEE']
+for key in dict1.keys():
+    if key not in badwords:
+        print key.encode('utf-8'),':',dict1[key]
+        
+        
+test1 = u'刘超喜欢学习'
+test2 = u'学习喜欢刘超'
+
+def segmentation(sentence,lists=[]):
+    jieba.suggest_freq(u"刘超", True)
+    #分词
+    sentence = jieba.cut(sentence,cut_all=False,HMM=False)
+    #组合
+    format_sentence=",".join(sentence)
+    lists=format_sentence.split(",")     
+    return lists
+
+test1 = segmentation(test1)
+test2 = segmentation(test2)
 
 #比较两个数列，二元语法
 def compareList(ori_list,test_list):
@@ -46,6 +78,8 @@ def compareList(ori_list,test_list):
                     count_list[i]+=1
     return count_list
 
+print compareList(t,test1)
+print compareList(t,test2)
 
 #计算概率    
 def probability(test_list,count_list,ori_dict):
@@ -54,33 +88,8 @@ def probability(test_list,count_list,ori_dict):
     p=1
     for key in test_list: 
         #数据平滑处理：加1法
-        p*=(float(count_list[flag]+1)/float(ori_dict.get(key,0)+1))
+        p*=(float(count_list[flag]+1)/float(ori_dict[key]+1))
         flag+=1
     return p
-
-
-if __name__ == "__main__":
-
-    #语料句子
-    sentence_ori="研究生物很有意思。他大学时代是研究生物的。生物专业是他的首选目标。他是研究生。"
-    ori_list=[]
-    ori_dict={}
-    sentence_ori_temp=""
-
-    #测试句子
-    sentence_test="他是研究生物的"
-    sentence_test_temp="" 
-    test_list=[]
-    count_list=[]
-    p=0
-
-    #分词并将结果存入一个list，词频统计结果存入字典
-    sentence_ori_temp=reform(sentence_ori)
-    ori_list=segmentation(sentence_ori_temp,ori_list,ori_dict)
-
-    sentence_test_temp=reform(sentence_test)
-    test_list=segmentation(sentence_test_temp,test_list)
-
-    count_list=compareList(ori_list, test_list)
-    p=probability(test_list,count_list,ori_dict)
-    print(p)
+print probability(test1,compareList(t,test1),dict1)
+print probability(test2,compareList(t,test2),dict1)
