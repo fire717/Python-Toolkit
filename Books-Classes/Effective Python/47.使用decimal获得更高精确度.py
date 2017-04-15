f415bb9ead7b8752a1457dccc6ee887af46e7230
Fire_Yw <fire15@126.com>
2017-04-15 09:18:50 +0800

    Create 47.使用decimal获得更高精确度.py

---
+++ Books-Classes/Effective Python/47.使用decimal获得更高精确度.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from decimal import Decimal
+
+rate = Decimal('1.45')
+print rate
+'''
+对于编程中可能用到的每一种数值，我们都可以拿对应的python内置类型或内置模块中的类表示
+
+Decimal类非常适合对精度要求很高，且对舍入行为要求很严的场合，例如货币计算等。
+'''
