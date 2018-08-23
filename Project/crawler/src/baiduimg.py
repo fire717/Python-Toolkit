f29396d6d54b6d71252c834ae7795147c76a5086
Fire_Yw <fire15@126.com>
2018-08-23 14:15:11 +0800

    Create baiduimg.py

---
+++ Project/crawler/src/baiduimg.py
@@ -0,0 +1,74 @@
+# 爬取百度图片
+import requests
+import os
+
+def getManyPages(keyword,pages):
+    params=[]
+    for i in range(30,30*pages+30,30):
+        params.append({
+                      'tn': 'resultjson_com',
+                      'ipn': 'rj',
+                      'ct': 201326592,
+                      'is': '',
+                      'fp': 'result',
+                      'queryWord': keyword,
+                      'cl': 2,
+                      'lm': -1,
+                      'ie': 'utf-8',
+                      'oe': 'utf-8',
+                      'adpicid': '',
+                      'st': -1,
+                      'z': '',
+                      'ic': 0,
+                      'word': keyword,
+                      's': '',
+                      'se': '',
+                      'tab': '',
+                      'width': '',
+                      'height': '',
+                      'face': 0,
+                      'istype': 2,
+                      'qc': '',
+                      'nc': 1,
+                      'fr': '',
+                      'pn': i,
+                      'rn': 30,
+                      'gsm': '1e',
+                      '1488942260214': ''
+                  })
+    url = 'https://image.baidu.com/search/acjson'
+    urls = []
+    for i in params:
+        try:
+            urls.append(requests.get(url,params=i).json().get('data'))
+        except:
+            print ('requests.exceptions.ConnectionError')
+
+    return urls
+
+
+def getImg(dataList, localPath):
+
+    if not os.path.exists(localPath):  # 新建文件夹
+        os.mkdir(localPath)
+
+    x = 0
+    for list in dataList:
+        for i in list:
+            if i.get('thumbURL') != None:
+                print('正在下载：%s' % i.get('thumbURL'))
+                ir = requests.get(i.get('thumbURL'))
+                open(localPath + '%d.jpg' % x, 'wb').write(ir.content)
+                x += 1
+            else:
+                print('图片链接不存在')
+
+if __name__ == '__main__':
+    #name_list = ['马路','城市','城市道路','乡村道路','柏油路','交通','汽车','新车','车流',]
+    #name_list = ['车站','车展','公交车','赛车']
+    #name_list = ['立交桥','高速公路']
+    name_list = ['车牌']
+    for name in name_list:
+    #name = '城市'
+      dataList = getManyPages(name, 10)  # 参数1:关键字，参数2:要下载的页数
+      getImg(dataList,'./locateBG/%s/' % name) # 参数2:指定保存的路径
