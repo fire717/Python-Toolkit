46b195da9a594c95692de44e8c7ecb2c9176dc37
Fire_Yw <fire15@126.com>
2019-07-19 23:46:36 +0800

    Update 平滑背景找目标边界.py

--- tools/OpenCV/平滑背景找目标边界.py
+++ tools/OpenCV/平滑背景找目标边界.py
@@ -6,6 +6,7 @@ import os
 
 
 def findBox(img):
+    h, w, c = img.shape
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     #cv2.imwrite('gray.jpg', gray)
 
@@ -16,11 +17,11 @@ def findBox(img):
 
     gradient = cv2.subtract(gradX, gradY)
     gradient = cv2.convertScaleAbs(gradient)  #求梯度
-    #cv2.imwrite('gradient.jpg', gradient)
+    cv2.imwrite('1gradient.jpg', gradient)
 
     blurred = cv2.GaussianBlur(gradient, (9, 9),0)
     (_, thresh) = cv2.threshold(blurred, 20, 255, cv2.THRESH_BINARY)
-    #cv2.imwrite('thresh.jpg', thresh)
+    cv2.imwrite('2thresh.jpg', thresh)
 
 
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (25, 25))
@@ -28,31 +29,26 @@ def findBox(img):
 
     closed = cv2.erode(closed, None, iterations=4)
     closed = cv2.dilate(closed, None, iterations=4)
-    #cv2.imwrite('closed.jpg', closed)
+    cv2.imwrite('3closed.jpg', closed)
 
     (_, cnts, _) = cv2.findContours(closed,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
 
     c = sorted(cnts, key=cv2.contourArea, reverse=True)[0]
 
-
     rect = cv2.minAreaRect(c)
     box = np.int0(cv2.boxPoints(rect))
 
-    # draw a bounding box arounded the detected barcode and display the image
     draw_img = cv2.drawContours(img.copy(), [box], -1, (0, 0, 255), 3)
-    #cv2.imwrite('draw_img.jpg', draw_img)
+    cv2.imwrite('4draw_img.jpg', draw_img)
 
-    #cv2.imshow("draw_img", draw_img)
     Xs = [i[0] for i in box]
     Ys = [i[1] for i in box]
-    x1 = min(Xs)
-    x2 = max(Xs)
-    y1 = min(Ys)
-    y2 = max(Ys)
-    hight = y2 - y1
-    width = x2 - x1
-    crop_img= img[y1:y1+hight, x1:x1+width]
+    x1 = max(0, min(Xs))
+    x2 = min(w, max(Xs))
+    y1 = max(0, min(Ys))
+    y2 = min(h, max(Ys))
+    crop_img= img[y1:y2, x1:x2]
 
     return crop_img
 
