48e2d530c1426643c6d50e322cace5e2da2eda55
Fire_Yw <fire15@126.com>
2020-02-27 10:27:23 +0800

    Create 人脸检测.py

---
+++ tools/OpenCV/人脸检测.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+
+detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+cap = cv2.VideoCapture("yx/test.mp4")#cv2.VideoCapture(0)
+
+while True:
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = detector.detectMultiScale(gray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+    cv2.imshow('frame', img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
