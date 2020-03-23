4c9a6e09f5c056d2e082ff76943e95140c25f341
Fire_Yw <fire15@126.com>
2020-03-23 19:15:51 +0800

    Update RGBpoint2HSV_HSL_nocv2.py

--- tools/OpenCV/RGBpoint2HSV_HSL_nocv2.py
+++ tools/OpenCV/RGBpoint2HSV_HSL_nocv2.py
@@ -37,9 +37,14 @@ def RGB2HSL(r, g, b):
         h = ((b-r)/m)*60 + 120
     elif mx == b:
         h = ((r-g)/m)*60 + 240
-    if mx == 0:
-        s = 0
-    else:
-        s = m/mx
+
     l = (mx+mn)/2.0
+
+    if mx == 0 or mx == mn:
+        s = 0
+    elif 0<l<0.5:
+        s = (mx-mn)*1.0/(2*l)
+    elif l>0.5:
+        s = (mx-mn)*1.0/(2-2*l)
+    
     return h, s, l
