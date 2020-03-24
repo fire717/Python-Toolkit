7a650bd081a7e99070fde69c8dd89820ac04eea8
Fire_Yw <fire15@126.com>
2020-03-24 09:45:35 +0800

    Update and rename RGBpoint2HSV_HSL_nocv2.py to RGB_HSV_HSL_nocv2.py

--- tools/OpenCV/RGB_HSV_HSL_nocv2.py
+++ tools/OpenCV/RGB_HSV_HSL_nocv2.py
@@ -48,3 +48,27 @@ def RGB2HSL(r, g, b):
         s = (mx-mn)*1.0/(2-2*l)
 
     return h, s, l
+
+
+def HSV2RGB(h,s,v):
+    assert 0<=h<360
+    assert 0<=s<=1
+    assert 0<=v<=1
+    c = v*s
+    x = c*(1-abs((h/60)%2-1))
+    m = v-c
+    if h<60:
+        r,g,b = c,x,0
+    elif h<120:
+        r,g,b = x,c,0
+    elif h<180:
+        r,g,b = 0,c,x
+    elif h<240:
+        r,g,b = 0,x,c
+    elif h<300:
+        r,g,b = x,0,c
+    elif h<360:
+        r,g,b = c,0,x
+
+    r,g,b = (r+m)*255, (g+m)*255, (b+m)*255
+    return r, g, b
