b1fc44c6ed6fe3c276493bf7f625b012066b73ff
Fire_Yw <fire15@126.com>
2020-07-03 00:13:56 +0800

    Create timecost.py

---
+++ CodePiece/timecost.py
@@ -0,0 +1,21 @@
+from functools import wraps
+
+def get_running_time(func):
+    # 使用wraps, 保证被装饰过的函数__name__的属性不变
+    @wraps(func)
+    def inner(*args, **kwargs):
+        if LOG_TIME:
+            start_time = time.time()
+        res = func(*args, **kwargs)
+        if LOG_TIME:
+            end_time = time.time()
+            print('The {} cost time : {}'.format(func.__name__, (end_time - start_time)))
+        return res
+    return inner
+
+
+@get_running_time
+def test():
+    print("test.")
+
+test()
